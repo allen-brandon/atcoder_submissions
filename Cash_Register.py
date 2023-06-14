14f08b13b6bd9179ce0d6c29b23bd44c81bede8d
Brandon Allen <me@brandon-allen.com>
2023-06-14 11:21:05 -0400

    Update Cash_Register.py

--- Cash_Register.py
+++ Cash_Register.py
@@ -1,9 +1,50 @@
-s = input()
-
-i, res = 0, 0
-while i < len(s):
-    if s[i] == '0' and i < len(s) and s[i+1] == '0':
-        i+=1
-    res+=1
-    i+=1
-print(res)
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "s = input()\n",
+    "\n",
+    "i, res = 0, 0\n",
+    "while i < len(s):\n",
+    "    if s[i] == '0' and i < len(s) and s[i+1] == '0':\n",
+    "        i+=1\n",
+    "    res+=1\n",
+    "    i+=1\n",
+    "print(res)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.10.9"
+  },
+  "orig_nbformat": 4
+ },
+ "nbformat": 4,
+ "nbformat_minor": 2
+}
